b5f48248a44f5da7add74e85c7ad21f09b07e1a0
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-06 20:24:01 +0300

    commit tree dde3f209122c3005907c6ae599e6eea59791e345

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <=400){
+    return "This one is on me!";
+  } 
+  else if (distance > 400 && distance <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500){
+    return "No can do.";
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return ("Bye.");
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+scuberGreetingForFeet(300)
+scuberGreetingForFeet(800)
+scuberGreetingForFeet(5600)
+ternaryCheckCity("NYC")
+switchOnCharmFromTip('generous')
+switchOnCharmFromTip('not as generous')
+switchOnCharmFromTip('something else')
